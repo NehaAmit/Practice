451a5c3905fc51c84e35dc4d8db304301175380a
NehaAmit <154895279+NehaAmit@users.noreply.github.com>
2024-03-06 12:46:07 +0530

    Update main.tf

--- .github/workflows/main.tf
+++ .github/workflows/main.tf
@@ -21,7 +21,6 @@ provider "aws" {
 resource "aws_instance" "app_server" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t3.micro"
-  key_name      = "My_Virginia_KeyPair"
 
   tags = {
     Name = var.ec2_name
